a27cbc8d7a1fe9f37893d21b58088e356a545eb8
Reut Mihai <reutmihai@icloud.com>
2024-08-26 12:51:13 +0300

    first exercise resolved

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerBackgroundChange = null;
+
+startBtn.addEventListener('click', 
+    () => {
+    timerBackgroundChange = setInterval(getBackgroundColor, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+
+stopBtn.addEventListener('click', () => {
+    stopInterval();
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
+
+
+function getBackgroundColor(){
+    document.body.style.background =
+      getRandomHexColor();
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function stopInterval() {
+  clearTimeout(timerBackgroundChange);
+}
