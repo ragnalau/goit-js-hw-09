c922ceea03c4fc2d614153ca69ff2613e7d62b10
ragnalau <105277921+ragnalau@users.noreply.github.com>
2024-08-26 09:51:35 +0000

    Deploying to gh-pages from @ ragnalau/goit-js-hw-09@a27cbc8d7a1fe9f37893d21b58088e356a545eb8 🚀

---
+++ 01-color-switcher.16ddacdc.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;function a(){document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{d=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearTimeout(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.16ddacdc.js.map
---
+++ 01-color-switcher.170fc466.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function a(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearTimeout(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.170fc466.js.map
